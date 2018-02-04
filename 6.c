c4bf2fd035ee61cd6c860b2e5066b677625bc7a8
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-02-04 09:12:00 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -7,10 +7,16 @@ clrscr();
 printf("Enter a b c values");
 scanf("%d%d%d",&a,&b,&c);
 if(a>b)&(a>c)
+{
 printf("\n a is big");
-else if (b>a)&(b>c)
+}
+else if(b>a)&(b>c)
+{
 printf("\n b is big");
+}
 else
+{
 printf("\n c is big");
+}
 getch();
 }
