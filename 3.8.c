9ef6e389a9c5570522392aeb17b380890337a5de
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-02-08 16:30:47 +0530

    Create 3.8.c

---
+++ 3.8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int num,i,a[i];
+  printf("enter the array element");
+	scanf("%d",&num);
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<num;i++)
+	{
+		printf("\n%d\t %d",a[i],i);
+	}
+	return 0;
+}
