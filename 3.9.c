34151f5780a3b9435c9990c0b5c27142f58a9be8
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-02-08 16:33:44 +0530

    Create 3.9.c

---
+++ 3.9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,hour,minute;
+clrscr();
+printf("enter the time is:");
+scanf("%d%d",&minute,&hour);
+a=hour/60;
+b=minute/60;
+time=minute%10;
+printf("the times are ");
+return 0;
+}
