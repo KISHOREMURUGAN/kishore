6014f41cc38c0db80f3bb84f11f2e304dd228cbf
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-02-08 16:46:40 +0530

    Create 4.3.c

---
+++ 4.3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[50];
+int c=0,a;
+clrscr();
+printf("\n Enter your string:");
+gets(str);
+for(a=0;str[a]!=0;a++)
+{
+if(str[a]==' ')
+{
+c++;
+}
+}
+printf("\n The number of spaces are%d:",c);
+return 0;
+}
