f21e6a9ddbe693beb525ef13112e2d2b24cd5e07
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-02-09 16:15:16 +0530

    Create 5.1.c

---
+++ 5.1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+char a[100]
+int k,i;
+clrscr();
+printf("ENTER THE STRING:");
+scanf("%s",&s);
+printf("enter number of times:");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("%s\n",s);
+}
+return 0;
+}
