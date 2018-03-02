61b33c22d5e048d4e420f4e527276e118ef4d7db
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-03-02 15:04:25 +0530

    Create 8.2.c

---
+++ 8.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a;
+scanf("%s",&a);
+if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u'))
+   {
+   printf(" is vowles ");
+   }
+else
+  {
+   printf("in is consonant");
+  }
+return 0;
+}
